5620c4da0e1708bef50d5bef91b1f530be422d54
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-11-13 20:19:31 +0900

    現在の天気を latlon ベースでも取得できるように変更

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -7,12 +7,18 @@ const BASE_URL
   = 'https://api.openweathermap.org/data/2.5/';
 const API_KEY = 'YOUR_API_KEY';
 
-function getCurrentWeatherEndpoint(query: string) {
-  return `${BASE_URL}weather?q=${query}`
+function getCurrentWeatherEndpoint(city: *) {
+  const { en, latitude, longitude } = city;
+  if (latitude && longitude) {
+    return `${BASE_URL}weather`
+      + `?lat=${latitude}&lon=${longitude}`
       + `&appid=${API_KEY}&lang=ja`;
+  }
+  return `${BASE_URL}weather?q=${en}`
+  + `&appid=${API_KEY}&lang=ja`;
 }
 
-function getCurrentWeather(city: string)
+function getCurrentWeather(city: *)
   : Promise<CurrentWeather> {
   const endpoint = getCurrentWeatherEndpoint(city);
   return fetch(endpoint)
